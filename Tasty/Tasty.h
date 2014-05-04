d844bbd8bb176eefbc52743e6fb53394d322dea2
Alexander Forselius <drsounds@gmail.com>
2014-05-04 09:45:02 +0200

    Initial Commit

---
+++ Tasty/Tasty.h
@@ -0,0 +1,13 @@
+//
+//  Tasty.h
+//  Tasty
+//
+//  Created by Alecca on 2014-05-04.
+//  Copyright (c) 2014 Alexander Forselius. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tasty : NSObject
+
+@end
---
+++ Tasty/Tasty.m
@@ -0,0 +1,13 @@
+//
+//  Tasty.m
+//  Tasty
+//
+//  Created by Alecca on 2014-05-04.
+//  Copyright (c) 2014 Alexander Forselius. All rights reserved.
+//
+
+#import "Tasty.h"
+
+@implementation Tasty
+
+@end
